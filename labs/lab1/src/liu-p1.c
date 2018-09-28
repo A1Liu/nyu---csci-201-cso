ad095fb11e9d812f2d206f213255db2ebb7332ee
Albert Liu <albertymliu@gmail.com>
2018-09-28 02:16:55 -0400

    finished parts 1 and 2

--- labs/lab1/src/liu-p1.c
+++ labs/lab1/src/liu-p1.c
@@ -1,20 +1,55 @@
-/*
+#include <stdio.h>
+#include <ctype.h>
+#define ARGCOUNT 3
 
-## Part 1
-Write a main program that has three command line arguments called
-`start`, `end`, and `incr`. All three are type `int`.
-Your program prints a series of numbers
-`start`, `start+incr`, `start+(2*incr)`, `...`, `end`.
-For example, if the three arguments are `2 10 3`, the values
-printed are `2 5 8 10`.
-Note from the example that the `end` value is printed even if it is
-not of the form `start` plus a multiple of `incr`.
+int parseInt(int *data, char * str);
+void output(int data[], int start, int end, int incr);
 
-You may assume for problem 1 that the arguments are legal, i.e.,
-`start<end` and `incr>0`.
+int main(int argc, char *argv[]) {
+	if (--argc != ARGCOUNT) return 2; // Error, wrong amount of arguments
+	int vars[ARGCOUNT];// Start, End, Incr
+	short success;
+	for (int data; argc > 0 && ( success = parseInt(&data,argv[argc]) ); argc--) {
+		vars[argc-1] = data;
+	}
+	if (!success) return 1; // one of the arguments wasn't correct
+	int start = vars[0], end = vars[1], incr = vars[2];
 
-*/
-
-int main(int argc, char *argv[argc]) {
+	int len = (end - start) / incr;
+	len += (end - start) % incr ? 2 : 1;
+	int out[len];
+	output(out,start,end,incr);
+	for (int i = 0; i < len; i++) {
+		printf("%d ",out[i]);
+	}
+	printf("\n");
 	return 0;
+}
+
+void output(int data[],int start, int end, int incr) {
+	int i = 0;
+	for ( ; start < end; start += incr)
+		data[i++] = start;
+	data[i] = end;
+}
+
+int parseInt(int *data,char *str) {// need to handle negatives
+	int sign=1;
+	*data = 0;
+	int success = 1;
+	char current = *str;
+
+	// Handle the first slot
+	if (isdigit(current))
+		*data = current - '0';
+	else if (current == '-')
+		sign =  -1;
+	else if (current == '+') ;
+	else return 0;
+	// Loop through the rest
+	for (int i = 1; (current = *(str+i)) && (success = isdigit(current)); i++) {
+		*data = *data * 10 + (current - '0');
+	}
+	*data *= sign;
+	return success;
 }
--- labs/lab1/src/liu-p2.c
+++ labs/lab1/src/liu-p2.c
@@ -1,6 +1,8 @@
 /*
 Write a main program that takes two command-line arguments,
-`lo` and `hi`. If `lo > hi`, `main()` prints an appropriate error message and terminates. Assuming `lo <= hi`, `main()` calls `isPrime(x)` for each `lo <= x <= hi`. For each *x*, `main()` prints one line which states either:
+`lo` and `hi`. If `lo > hi`, `main()` prints an appropriate error message and terminates.
+ Assuming `lo <= hi`, `main()` calls `isPrime(x)` for each `lo <= x <= hi`. For each *x*,
+ `main()` prints one line which states either:
 * *x* is invalid
 * *x* is prime
 * *x* is not prime
@@ -8,16 +10,67 @@ Write a main program that takes two command-line arguments,
 (You don't print a literal 'x' but instead the value of *x*).
 
 */
+#include <stdio.h>
+#include <ctype.h>
 
-int isPrime(int x);
+#define ARGCOUNT 2
+
+short isPrime(int x);
+short parseInt(int *data, char *str);
+char *getMessage(int n);
 
 int main(int argc, char *argv[argc]) {//Part B
+	if (--argc != ARGCOUNT) return 2; // Error, wrong amount of arguments
+	int vars[ARGCOUNT];// Start, End, Incr
+	short success;
+	for (int data; argc > 0 && ( success = parseInt(&data,argv[argc]) ); argc--) {
+		vars[argc-1] = data;
+	}
+	if (!success) return 1;
+	int lo = vars[0], hi = vars[1];
+	for (int x = lo; x <= hi; x++)
+		printf("%d %s\n", x, getMessage( x ) );
 	return 0;
 }
 
-int isPrime(int x) {//Part A
-	// If x<2 or x>1000, the input is erroneous and isPrime() returns -1.
-	// If 2<=x<=1000 and x is a prime number, isPrime() returns 1.
-	// If 2<=x<=1000 and x is not a prime number, isPrime() returns 0.
-	return 0;
+char *getMessage(int n) {
+	switch(isPrime(n)) {
+		case 1:
+			return "is prime";
+		case 0:
+			return "is not prime";
+		default:
+			return "is invalid";
+	}
+}
+short isPrime(int x) {//Part A
+	if (x < 2 || x > 1000) return -1;
+	static short done = 0;
+	static short composite[999];
+	if (done) return !composite[x-2];
+	for (int i = 2; i <= 1000; i++)
+		if (composite[i-2] == 0)
+			for (int j = i*2; j <= 1000; j+=i)
+				composite[j-2] = 1;
+	done = 1;
+	return !composite[x-2];
+}
+
+short parseInt(int *data,char *str) {
+	short sign=1;
+	*data = 0;
+	short success;
+	char current = *str;
+
+	// Handle the first slot
+	if ( ( success = isdigit(current) ) ) *data = current - '0';
+	else if (current == '-') sign =  -1;
+	else if (current != '+') return 0;
+
+	// Loop through the rest
+	for ( ; (current = *(++str)) && ( success = isdigit(current) ); ) {
+		*data = *data * 10 + (current - '0');
+	}
+	*data *= sign;
+	return success;//returns 1 for successful and 0 for failed
 }
