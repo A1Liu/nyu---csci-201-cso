3b143312144bc1bc70b66870ddd1b71df3c70fb1
Albert Liu <albertymliu@gmail.com>
2018-09-28 04:08:23 -0400

    added getch stuff

--- utils/parse.c
+++ utils/parse.c
@@ -2,7 +2,13 @@
 #include <ctype.h>
 #define ARGCOUNT 2
 #define ERROR_CODE 2
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int  bufp = 0;
 
+int  getch(void);
+void ungetch(int);
+int getint(int *pn);
 short parseInts(int argc, char *argv[argc], int vars[argc-1]);
 short parseInt(int *data, char *str);
 
@@ -22,7 +28,44 @@ short parseInts(int argc,char *argv[argc-1], int vars[argc]) {//argc and argv ar
 	return success;
 }
 
-short parseInt(int *data,char *str) {
+int getch(void) {
+	return (bufp>0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many chars\n");
+	else
+		buf[bufp++] = c;
+}
+
+int getint(int *pn) {// pn holds the data
+	int c, sign;
+	while (isspace(c=getch())) ; // Move to first non-space character
+	if (!isdigit(c) && c!=EOF && c!='+' && c!='-') {// If we're getting something that's a letter or something weird, put it back and return 0
+		ungetch(c);
+		return 0;
+	}
+
+	sign = (c=='-') ? -1 : 1; // Whether to be negative or positive
+	if (c=='+' || c=='-') {
+		c = getch();
+		if (!isdigit(c)) {
+			ungetch(c);
+			ungetch(sign == -1 ? '-' : '+');
+			return 0;
+		}
+	}// if it's a sign
+
+	 for (*pn = 0; isdigit(c); c=getch())
+	 	*pn = 10 * *pn + (c-'0');
+	 *pn *= sign;
+	 if (c != EOF)
+	 	ungetch(c);
+	 return c == 0 ? -1 : c;
+}
+
+short parseInt(int *data,char *str) {//For strings
 	short sign=1;
 	*data = 0;
 	short success;
